a08c25b68ef229bcc4837608c369a8e4ea9416d6
Salma Attia <salmamohamedattia@gmail.com>
2017-09-26 17:07:29 +0200

    last

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925160536) do
+ActiveRecord::Schema.define(version: 20170926134450) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170925160536) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
